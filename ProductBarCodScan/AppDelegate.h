793a0445a8d22b1f88fb8033ed3c1c808eecf2a0
Pawar, Santosh-CW <Santosh.Pawar@bestbuy.com>
2015-04-03 15:56:20 -0500

    Initial Commit

---
+++ ProductBarCodScan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProductBarCodScan
+//
+//  Created by Pawar, Santosh-CW on 4/3/15.
+//  Copyright (c) 2015 Pawar, Santosh-CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProductBarCodScan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProductBarCodScan
+//
+//  Created by Pawar, Santosh-CW on 4/3/15.
+//  Copyright (c) 2015 Pawar, Santosh-CW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
